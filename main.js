4bc15ac3e8aae7f093045e0a2502c0c29e12f5d9
Chimoha31 <mokochii1108@gmail.com>
2022-02-22 22:11:24 -0800

    fetch

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 'use strict';
 
 const products_container = document.querySelector('#products_container');
-const image_div = document.querySelector('#image');
-const products_detail = document.querySelector('#products_detail')
-const detail_div = document.createElement('div');
 
-
+// ----------------------
+// API 取得
+// ----------------------
 async function callAPI() {
   const res = await fetch('https://fakestoreapi.com/products');
   const products = await res.json();
   console.log(products);
+
+  products.map((product) => {
+    const img_div = document.createElement('div');
+    const img = document.createElement('img');
+    img_div.appendChild(img);
+    img.setAttribute('src', `${product.image}`);
+    products_container.appendChild(img_div);
+    console.log(products_container);
+  })
+
 }
 
 callAPI();
