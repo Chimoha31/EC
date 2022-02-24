9a092d8e940915cd4cecc8e9894e184daa215beb
Chimoha31 <mokochii1108@gmail.com>
2022-02-23 19:41:07 -0800

    DOM manipulate

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 'use strict';
 
-const products_container = document.querySelector('#products_container');
 
 // ----------------------
-// API 取得
+// API DOM
 // ----------------------
 async function callAPI() {
   const res = await fetch('https://fakestoreapi.com/products');
   const products = await res.json();
   console.log(products);
-
+  
+  const products_container = document.querySelector('#products_container');
   products.map((product) => {
-    const img_div = document.createElement('div');
+    // Item img
+    const div = document.createElement('div');
+    div.setAttribute('class', 'item')
     const img = document.createElement('img');
-    img_div.appendChild(img);
     img.setAttribute('src', `${product.image}`);
-    products_container.appendChild(img_div);
-    console.log(products_container);
-  })
+    div.appendChild(img);
+    products_container.appendChild(div);
+    
+    // Item title
+    const h4 = document.createElement('h4');
+    h4.textContent = product.title;
+    div.appendChild(h4);
 
+    // Item price
+    const p = document.createElement('p');
+    p.textContent = `$ ${product.price}`;
+    div.appendChild(p);
+  })
+  console.log(products_container);
 }
 
 callAPI();
